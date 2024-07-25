2f85f3c14d833803553c446e757902cb2270de3c
UznetDev <uznattv@gmail.com>
2024-07-25 16:04:11 +0500

    new update

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import pandas as pd
 import time
 from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 from loader import df, col
 import plotly.express as px
