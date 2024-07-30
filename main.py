48525ca873b9d3a4b11ae783ccb3c777d806fb9d
UznetDev <uznattv@gmail.com>
2024-07-30 21:58:25 +0500

    fix

--- main.py
+++ main.py
@@ -3,16 +3,20 @@ import time
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 from loader import df, col
-import plotly.express as px
-import plotly.graph_objects as go
-from sklearn.svm import SVR
 from function import *
-import numpy as np
-import plotly.graph_objects as go
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
+import plotly.graph_objects as go
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from loader import df
+import streamlit as st
 
 
 
@@ -23,13 +27,13 @@ st.set_page_config(
     initial_sidebar_state="expanded"
 )
 
-def stream_data():
-    st.sidebar.header("Input Parameters")
+st.sidebar.header("Input Parameters")
     
 
-    columns = st.sidebar.multiselect("Select Columns to Display", options=col, default=col)
+columns = st.sidebar.multiselect("Select Columns to Display", options=col, default=col)
 
-    st.markdown(
+
+st.markdown(
         """
         <style>
         .stSlider label {
@@ -41,12 +45,14 @@ def stream_data():
         unsafe_allow_html=True
     )
 
-    input_data = {}
-    for column in columns:
-        input_data[column] = st.sidebar.slider(f'Select value for ( {column} )',
+input_data = {}
+for column in columns:
+    input_data[column] = st.sidebar.slider(f'Select value for ( {column} )',
                                                min_value=0, 
                                                max_value=int(df[column].max()), 
                                                value=int(df[column].mean()))
+
+def stream_data():
 
 
     model, accuracy = train_model(df)
@@ -64,53 +70,68 @@ def stream_data():
             yield word + " "
             time.sleep(0.05)
 
-
-
     else:
         text = "\nPlease select values for all features to make a prediction.\n"
         for word in text.split(" "):
             yield word + " "
             time.sleep(0.02)
 
-    # eatures = ['pregnancies', 'diastolic', 'triceps', 'insulin', 'bmi', 'dpf', 'age']
-    features = columns
-
-    if features.count('glucose'):
-        features.remove('glucose')
-
-    if features:
-        X = df[features]
-        y = df['glucose']
-
-        scaler = MinMaxScaler()
-        X_scaled = scaler.fit_transform(X)
-        fig = go.Figure()
-
-        for i, feature in enumerate(features):
-            X_feature = X_scaled[:, i].reshape(-1, 1)
-            X_train, X_test, y_train, y_test = train_test_split(X_feature, y, random_state=0)
-            
-            model = LinearRegression()
-            model.fit(X_train, y_train)
-            
-            x_range = np.linspace(X_feature.min(), X_feature.max(), 100)
-            y_range = model.predict(x_range.reshape(-1, 1))
-            
-            fig.add_trace(go.Scatter(x=X_train.squeeze(), y=y_train, name=f'Train {feature}', mode='markers', marker=dict(opacity=0.5)))
-            fig.add_trace(go.Scatter(x=X_test.squeeze(), y=y_test, name=f'Test {feature}', mode='markers', marker=dict(opacity=0.5)))
-            
-            fig.add_trace(go.Scatter(x=x_range, y=y_range, name=f'Regression {feature}', mode='lines'))
-
-        fig.update_layout(
-            xaxis_title="Normalized Feature Value",
-            yaxis_title="Glucose",
-            legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
-        )
-
-
-        st.plotly_chart(fig)
 
 
 st.write_stream(stream_data)
 
+features = columns
+
+if features.count('glucose'):
+    features.remove('glucose')
+cols = st.columns(2)
+if features:
+    X = df[features]
+    y = df['glucose']
+
+    scaler = MinMaxScaler()
+    X_scaled = scaler.fit_transform(X)
+    fig = go.Figure()
+
+    for i, feature in enumerate(features):
+        X_feature = X_scaled[:, i].reshape(-1, 1)
+        X_train, X_test, y_train, y_test = train_test_split(X_feature, y, random_state=0)
+
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        x_range = np.linspace(X_feature.min(), X_feature.max(), 100)
+        y_range = model.predict(x_range.reshape(-1, 1))
+
+        fig.add_trace(go.Scatter(x=X_train.squeeze(), y=y_train, name=f'Train {feature}', mode='markers',
+                                 marker=dict(opacity=0.5)))
+        fig.add_trace(
+            go.Scatter(x=X_test.squeeze(), y=y_test, name=f'Test {feature}', mode='markers', marker=dict(opacity=0.5)))
+
+        fig.add_trace(go.Scatter(x=x_range, y=y_range, name=f'Regression {feature}', mode='lines'))
+
+    fig.update_layout(
+        xaxis_title="Normalized Feature Value",
+        yaxis_title="Glucose",
+        legend=dict(orientation="h", yanchor="bottom", y=1.02, xanchor="right", x=1)
+    )
+
+    cols[0].plotly_chart(fig)
+
+fig = px.scatter_3d(df, x='insulin', y='dpf', z='glucose',
+                    color='glucose', title='3D Scatter Plot of Insulin, Glucose, and DPF')
+
+X = df[['insulin', 'dpf']]
+y = df['glucose']
+model = LinearRegression()
+model.fit(X, y)
+
+insulin_range = np.linspace(X['insulin'].min(), X['insulin'].max(), 50)
+glucose_range = np.linspace(X['dpf'].min(), X['dpf'].max(), 50)
+insulin_grid, glucose_grid = np.meshgrid(insulin_range, glucose_range)
+dpf_pred = model.predict(np.c_[insulin_grid.ravel(), glucose_grid.ravel()]).reshape(insulin_grid.shape)
+
+fig.add_trace(go.Surface(x=insulin_grid, y=glucose_grid, z=dpf_pred, colorscale='Viridis', opacity=0.5))
+
+cols[1].write(fig)
 
